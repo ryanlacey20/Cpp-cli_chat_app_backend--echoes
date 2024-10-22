e987c7ed9e6224d2b7ef98594996ba9e5c10e04f
Ryan L <ryanpatricklacey@gmail.com>
2024-10-22 16:20:49 +0100

    wip creating HTTP requests to Firebase DB

--- src/server.cpp
+++ src/server.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
+#include <curl/curl.h>
 #include "crow_all.h"
-#include "dbConnection.h"
-#include <sql.h>
-#include <sqlext.h>
-#include <string>
-#include <vector>
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
-int startServer()
-{
+static size_t MyWriteCallback(void* data, size_t elementSize, size_t elementCount, std::string* buffer) {
+    buffer->append((char*)data, elementSize * elementCount);
+    return elementSize * elementCount;
+}
+
+
+json getTodaysMessage() {
+    CURL* curl = curl_easy_init();
+    std::string readBuffer;
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages.json");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
+    return json::parse(readBuffer);
+}
+
+int startServer() {
     crow::SimpleApp app;
-    Database dbHandler;
 
-    CROW_ROUTE(app, "/getMessages")
-    ([&dbHandler](const crow::request &req)
-     {
-         json results = dbHandler.executeQuery("SELECT * FROM messages ORDER BY created_at");
-
-    // return crow::response(results.dump()); // .dump is converting to a string, not sure if we need or would be better/possible to just return a json..
-        return "hello"; });
+    CROW_ROUTE(app, "/getMessages")([&]() {
+        return crow::response(getTodaysMessage().dump());
+    });
 
     app.port(18080).multithreaded().run();
-
     return 0;
+
 }
+
+// curl -X PUT -d '{ “m1": { "user": "Jack", "message": "hello world" }, “m2": { "user": "Sparrow", "message": "how are you" } }' 'https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages.json'
