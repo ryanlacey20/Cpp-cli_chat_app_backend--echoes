f69effd17b90155fc53ce124b68c4a9a7a8d6725
Ryan L <ryanpatricklacey@gmail.com>
2024-10-23 16:17:23 +0100

    wip : putMessage
    working on putMessage endpoint to receive a message, extract relevant data and add it to DB

--- src/server.cpp
+++ src/server.cpp
@@ -2,13 +2,42 @@
 #include <curl/curl.h>
 #include "crow_all.h"
 #include <nlohmann/json.hpp>
+#include <chrono>
+
+
 
 using json = nlohmann::json;
+
+const std::string generalMessagesDBEndpointURL = "https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages/";
+
+std::string getCurrentDate() {
+    // get curr time
+    auto now = std::chrono::system_clock::now();
+    
+    // use time_t for calendar time
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    
+    // tm struct
+    std::tm now_tm = *std::localtime(&now_c);
+    
+    // format YYYYMMDD
+    std::ostringstream oss;
+    oss << std::put_time(&now_tm, "%Y%m%d");
+    
+    return oss.str();
+}
+std::string parseTodaysDBURL(){
+    std::ostringstream urlOss;
+    urlOss << "https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages/" << getCurrentDate() << ".json";
+    return urlOss.str();
+}
 
 static size_t WriteCallback(void* data, size_t size, size_t nmemb, std::string* buffer) {
     buffer->append(static_cast<char*>(data), size * nmemb);
     return size * nmemb;
 }
+
+
 
 json getFirebaseData(const std::string& url) {
     CURL* curl = curl_easy_init();
@@ -24,8 +53,7 @@ json getFirebaseData(const std::string& url) {
     return json::parse(readBuffer); 
 }
 
-bool
- putFirebaseData(const std::string& url, const json& data){
+bool putFirebaseData(const std::string& url, const json& data){
     CURL* curl = curl_easy_init();
     if (!curl) return false; 
 
@@ -41,52 +69,70 @@ bool
 
     return res == CURLE_OK; 
 }
-
+void debugResetFirbaseDB(json data){
+    putFirebaseData(generalMessagesDBEndpointURL, data);
+}
 int startServer() {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/getMessages")([&]() {
-        return crow::response(getFirebaseData("https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages/0.json").dump());
+    CROW_ROUTE(app, "/testDev")([&]() {
+        std::cout << __cplusplus << std::endl;
+        return crow::response(getCurrentDate());
     });
 
-    CROW_ROUTE(app, "/putMessage")([&]() {
+    CROW_ROUTE(app, "/getTodaysMessages")([&]() {
 
-            const json data = {
-                {"1", {   
-                    {"user", "Commdus"},
-                    {"message", "MEET ME AFTER THE TOGA PARTY."},
-                    {"timestamp", "2024-10-22T12:34:56Z"}
-                    }
-                },
-                {"2", {
-                    {"user", "Maximus"},
-                    {"message", "ARE YOU NOT ENTERTAINED?"},
-                    {"timestamp", "2024-10-22T12:35:00Z"}
-                    }
-                }
-        };
+        std::string todaysMessagesDBEndpointURL = parseTodaysDBURL();
+        return crow::response(getFirebaseData(todaysMessagesDBEndpointURL).dump());
 
-        //  ---------- MSG JSON DATA STRUCTURE --------------
-   
-        // {
-        //         {"1", {   
-        //             {"user", "Russel"},
-        //             {"message", "MEET ME APRES TOGA PARTY!"},
-        //             {"timestamp", "2024-10-22T12:34:56Z"}
+    });
+
+// left off here, succesfully reads data from body of request
+// TODO: get MD of messages to find what the Id of this message will be or else find a way to auto index the data added to the DB, the latter would be preferable
+
+    CROW_ROUTE(app, "/putMessage").methods("PUT"_method)([](const crow::request& req) {
+        auto jsonData = json::parse(req.body);
+
+//  -----DEV WORK-----
+//  ---------- MSG JSON DATA STRUCTURE --------------
+        //     const json data = {
+        //         {"20241023", 
+        //             {
+        //                 {"md", {
+        //                     {"length", 2}
+        //                     }
+        //                 },
+        //                 {"m1", {   
+        //                     {"user", "Commodus"},
+        //                     {"message", "MEET ME AFTER THE TOGA PARTY."},
+        //                     {"timestamp", "2024-10-22T12:34:56Z"}
+        //                     }
+        //                 },
+        //                 {"m2", {
+        //                     {"user", "Maximus"},
+        //                     {"message", "ARE YOU NOT ENTERTAINED?"},
+        //                     {"timestamp", "2024-10-22T12:35:00Z"}
+        //                     }
+        //                 }
         //             }
-        //         },
-        //         {"2", {
-        //             {"user", "Crow"},
-        //             {"message", "ARE YOU NOT ENTERTAINED?"},
-        //             {"timestamp", "2024-10-22T12:35:00Z"}
-        //             }
-        //         }
+        //     }
         // };
 
+        // const json msgData = 
+        //     {
+        //         {"user", "Paul"},
+        //         {"message", "Hello World."},
+        //         {"timestamp", "2024-10-22T12:34:56Z"}
+        //     };
 
-        
-        putFirebaseData("https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages.json", data);
-        return crow::response("ran, check DB");
+//  -----DEV WORK-----
+
+
+
+        //debugResetFirbaseDB(data);
+        std::string todaysMessagesDBEndpointURL = parseTodaysDBURL();
+        // putFirebaseData(todaysMessagesDBEndpointURL, msgData);
+        return crow::response(jsonData.dump());
 
     });
 
