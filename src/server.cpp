53d642c95b2f4af2198276b07e6dafe635cc7993
Ryan L <ryanpatricklacey@gmail.com>
2024-10-23 11:49:06 +0100

    basic getFunctionality implemented

--- src/server.cpp
+++ src/server.cpp
@@ -5,37 +5,33 @@
 
 using json = nlohmann::json;
 
-static size_t MyWriteCallback(void* data, size_t elementSize, size_t elementCount, std::string* buffer) {
-    buffer->append((char*)data, elementSize * elementCount);
-    return elementSize * elementCount;
+static size_t WriteCallback(void* data, size_t size, size_t nmemb, std::string* buffer) {
+    buffer->append(static_cast<char*>(data), size * nmemb);
+    return size * nmemb;
 }
 
-
-json getTodaysMessage() {
+json getFirebaseData(const std::string& url) {
     CURL* curl = curl_easy_init();
+    if (!curl) return json(); 
+
     std::string readBuffer;
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
 
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages.json");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    }
+    return json::parse(readBuffer); 
 
-    return json::parse(readBuffer);
 }
 
-int startServer() {
+int startServer(const std::string& url) {
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/getMessages")([&]() {
-        return crow::response(getTodaysMessage().dump());
+        return crow::response(getFirebaseData("https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages.json").dump());
     });
 
     app.port(18080).multithreaded().run();
     return 0;
-
 }
-
-// curl -X PUT -d '{ “m1": { "user": "Jack", "message": "hello world" }, “m2": { "user": "Sparrow", "message": "how are you" } }' 'https://echoes-86d80-default-rtdb.europe-west1.firebasedatabase.app/messages.json'
